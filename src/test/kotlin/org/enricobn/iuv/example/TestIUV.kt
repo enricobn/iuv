c060d6aa2a8cdbecadf0dff1f5102d404e0040e6
Enrico Benedetti <enrico_benedetti@yahoo.com>
2017-05-21 21:06:37 +0200

    More on TestIUV.

--- src/test/kotlin/org/enricobn/iuv/example/TestIUV.kt
+++ src/test/kotlin/org/enricobn/iuv/example/TestIUV.kt
@@ -8,23 +8,31 @@ import org.enricobn.iuv.IUV
 import org.enricobn.iuv.Message
 import org.enricobn.iuv.MessageBus
 
-data class TestModel(val button1Model: ButtonModel,
-                     val button2Model: ButtonModel)
+data class TestModel(val buttonModels: List<ButtonModel>)
 
 class TestIUV : IUV<TestModel>() {
     private val height = 50
     private val width = 10
-    private val button1 = ButtonComponent("Button 1")
-    private val button2 = ButtonComponent("Button 2")
+    private val buttons = (1..height).map { y ->
+        (1..width).map { x ->
+            ButtonComponent("Button " + index(y, x))
+        }
+    }.flatten()
 
     override fun init(): TestModel {
-        return TestModel(ButtonModel(false), ButtonModel(false))
+        return TestModel((1..height).map {
+            (1..width).map { ButtonModel(false) }
+        }.flatten())
     }
 
     override fun update(message: Message, model: TestModel): TestModel {
-        val button1Model = button1.update(message, model.button1Model)
-        val button2Model = button2.update(message, model.button2Model)
-        return TestModel(button1Model, button2Model)
+        val buttonModels = (1..height).map { y ->
+            (1..width).map { x ->
+                buttons[index(y, x)].update(message, model.buttonModels[index(y, x)])
+            }
+        }.flatten()
+
+        return TestModel(buttonModels)
     }
 
     override fun view(messageBus: MessageBus, model: TestModel): HtmlBlockTag.() -> Unit = {
@@ -32,14 +40,16 @@ class TestIUV : IUV<TestModel>() {
             for (y in 1..height) {
                 tr {
                     for (x in 1..width) {
+                        val index = index(y, x)
                         td {
-                            button1.render(this, messageBus, model.button1Model)
-                            button2.render(this, messageBus, model.button2Model)
+                            buttons[index].render(this, messageBus, model.buttonModels[index])
                         }
                     }
                 }
             }
         }
     }
+
+    private fun index(y: Int, x: Int) = (y - 1) * width + x - 1
 
 }
