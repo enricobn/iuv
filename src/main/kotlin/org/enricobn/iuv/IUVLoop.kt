6598ffabc384989be711c5b93a5804c409f1f577
Enrico Benedetti <enrico_benedetti@yahoo.com>
2017-06-09 21:22:54 +0200

    More on snabbdom.

--- src/main/kotlin/org/enricobn/iuv/IUVLoop.kt
+++ src/main/kotlin/org/enricobn/iuv/IUVLoop.kt
@@ -4,36 +4,105 @@ import kotlinx.html.div
 import kotlinx.html.dom.create
 import org.w3c.dom.HTMLElement
 import org.enricobn.iuv.impl.MessageBusImpl
+import org.w3c.dom.Element
+import org.w3c.dom.Node.Companion.TEXT_NODE
+import org.w3c.dom.Text
+import org.w3c.dom.get
 import kotlin.browser.document
 
 class IUVLoop<MODEL>(private val iuv: IUV<MODEL>) {
     private var model = iuv.init()
     private val messageBus = MessageBusImpl(this::onMessage)
     private var view : HTMLElement? = null
+    private var viewH : dynamic = null
 
     fun run() {
-        updateDocument()
+        view = document.create.div()
+        document.body!!.appendChild(view!!)
+        updateDocument(true)
     }
 
     fun onMessage(message: Message) {
         model = iuv.update(message, model)
-        updateDocument()
+        updateDocument(false)
     }
 
-    private fun updateDocument() {
+    private fun updateDocument(first: Boolean) {
         val newView = document.create.div {
             iuv.view(messageBus, model).invoke(this)
         }
 
-        val lastView = view
+        val newH = toH(newView)
 
-        if (lastView is HTMLElement) {
-            lastView.attributes.length
-            lastView.remove()
+        if (first) {
+            patch(view!!, newH)
+        } else {
+            patch(viewH, newH)
         }
 
         view = newView
-        document.body!!.appendChild(view!!)
+        viewH = newH
+    }
+
+}
+
+private fun toH(element: Element) : dynamic {
+    val name = element.nodeName
+    val data : dynamic = object {}
+
+//    console.log("name $name")
+
+    if (element.attributes.length > 0) {
+        val attributes : dynamic = object {}
+        for (i in 0..element.attributes.length - 1) {
+            val attr = element.attributes.get(i)
+            val attrName = attr?.name
+            val attrValue = attr?.value
+            attributes[attrName] = attrValue
+//            console.log("$attrName : $attrValue")
+        }
+        data["attrs"] = attributes
+    }
+
+    if (element is HTMLElement) {
+        element.onclick?.let {
+//            console.log("onclick")
+            data["on"] = object {
+                val click = arrayOf(element.onclick!!)
+            }
+        }
+    }
+
+    if (element.nodeType == TEXT_NODE) {
+        console.log("TEXT_NODE")
+        return "element.innerText"
+    } else {
+        val children = mutableListOf<dynamic>()
+
+//        for (i in 0..element.children.length - 1) {
+//            val child = element.children.get(i)
+//
+//            if (child is HTMLElement) {
+//                children.add(toH(child))
+//            } else {
+//                console.log("no HTMLElement")
+//            }
+//        }
+
+        for (i in 0..element.childNodes.length - 1) {
+            val child = element.childNodes.get(i)
+
+            if (child is Element) {
+                children.add(toH(child))
+            } else if (child is Text) {
+                children.add(child.wholeText)
+            } else {
+                console.log("unknown type")
+                console.log(child)
+            }
+        }
+
+        return h(name, data, children.toTypedArray())
     }
 
 }
--- src/test/kotlin/org/enricobn/iuv/example/testmain.kt
+++ src/test/kotlin/org/enricobn/iuv/example/testmain.kt
@@ -1,43 +1,24 @@
 package org.enricobn.iuv.example
 
+import org.enricobn.iuv.IUVLoop
 import org.enricobn.iuv.h
 import org.enricobn.iuv.patch
 import kotlin.browser.document
-import kotlin.js.json
 
 fun main(args: Array<String>) {
-    val vnode = h("span", "foobar")
-    val container = document.body
-
-    patch(container!!, vnode)
-
-//    val newVnode = h("div", js("""[
-//        h('a', {on: {click: [clickHandler, 1]}, attrs: {href: '#'}}, '1'),
-//        h('a', {on: {click: [clickHandler, 2]}, attrs: {href: '#'}}, '2'),
-//        h('a', {on: {click: [clickHandler, 3]}, attrs: {href: '#'}}, '3'),
-//        ]"""))
-
-    val newVnode = h("div", arrayOf(a(1, ::clickHandler), a(2, ::clickHandler), a(3, ::clickHandler)))
-
-    patch(vnode, newVnode)
-
-    val j = json(
-            Pair("on", json(
-                        Pair("click", arrayOf(::clickHandler, 1))
-                       )
-            ),
-            Pair("attrs", json(Pair("href", "#")))
-    )
-
-    var o : dynamic = object {}
-
-    o["pippo"]["caio"] = "pluto"
-
-
-//    val vu = TestIUV()
+//    val vnode = h("span", "foobar")
+//    val container = document.body
 //
-//    val loop = IUVLoop(vu)
-//    loop.run()
+//    patch(container!!, vnode)
+//
+//    val newVnode = h("div", arrayOf(a(1, ::clickHandler), a(2, ::clickHandler), a(3, ::clickHandler)))
+//
+//    patch(vnode, newVnode)
+
+    val vu = TestIUV()
+
+    val loop = IUVLoop(vu)
+    loop.run()
 }
 
 
