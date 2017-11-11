76a19c5ac6556acb954dbe1265badecec1d6a554
Enrico Benedetti <enrico_benedetti@yahoo.com>
2017-11-11 13:24:53 +0100

    More on Material.

--- examples/buttons/src/main/kotlin/org/iuv/examples/components/Material.kt
+++ examples/buttons/src/main/kotlin/org/iuv/examples/components/Material.kt
@@ -20,3 +20,36 @@ fun <MESSAGE> HTML<MESSAGE>.mtButton(init: ButtonH<MESSAGE>.() -> Unit) {
 
     add(button)
 }
+
+fun <MESSAGE> HTML<MESSAGE>.mtCard(title: String,text: String) {
+        div {
+            classes ="demo-card-square mdl-card mdl-shadow--2dp"
+            div {
+                classes ="mdl-card__title mdl-card--expand"
+                +title
+            }
+            div {
+                classes ="mdl-card__supporting-text"
+                +text
+            }
+            div {
+                classes ="mdl-card__actions mdl-card--border"
+                +"Border"
+            }
+        }
+//    <div class="demo-card-square mdl-card mdl-shadow--2dp">
+//    <div class="mdl-card__title mdl-card--expand">
+//    <h2 class="mdl-card__title-text">Update</h2>
+//    </div>
+//    <div class="mdl-card__supporting-text">
+//    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+//    Aenan convallis.
+//    </div>
+//    <div class="mdl-card__actions mdl-card--border">
+//    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+//    View Updates
+//    </a>
+//    </div>
+//    </div>
+}
+
