98f6f8950f3ff16f07a2ecc098f9827a1e1d77ee
Enrico Benedetti <enrico_benedetti@yahoo.com>
2018-04-03 13:11:04 +0200

    Cleanup.

--- examples/buttons/src/main/kotlin/org/iuv/examples/buttons/ButtonsMain.kt
+++ examples/buttons/src/main/kotlin/org/iuv/examples/buttons/ButtonsMain.kt
@@ -2,13 +2,13 @@ package org.iuv.examples.buttons
 
 import org.iuv.core.IUVApplication
 import org.iuv.core.SnabbdomRenderer
-import org.iuv.examples.buttons.ButtonsIUV
-import org.iuv.examples.buttons.PostServiceImpl
 
 @JsName("ButtonsMain")
 class ButtonsMain {
+
     fun run() {
         val application = IUVApplication(ButtonsIUV(1, PostServiceImpl()), SnabbdomRenderer())
         application.run()
     }
+
 }
