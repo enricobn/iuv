dc176bc1052713d089226a66436137a12bafb267
Enrico Benedetti <enrico_benedetti@yahoo.com>
2018-04-05 13:28:01 +0200

    Refactoring.

--- iuv-core/src/main/kotlin/org/iuv/core/UV.kt
+++ iuv-core/src/main/kotlin/org/iuv/core/UV.kt
@@ -2,12 +2,15 @@ package org.iuv.core
 
 import org.w3c.xhr.XMLHttpRequest
 
+// TODO adjust url with timestamp to bypass the cache:
+// oReq.open("GET", url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime());
+// TODO handle failure && UNSENT state
 class GetAsync<in J, out MESSAGE>(private val url: String, private val handler: (J) -> MESSAGE) : Cmd<MESSAGE> {
 
     override fun run(messageBus: MessageBus<MESSAGE>) {
         val request = XMLHttpRequest()
         request.onreadystatechange = { _ ->
-            if (request.readyState.toInt() == 4 && request.status.toInt() == 200) {
+            if (request.readyState == XMLHttpRequest.DONE && request.status.toInt() == 200) {
                 val response = kotlin.js.JSON.parse<J>(request.responseText)
                 messageBus.send(handler(response))
             }
