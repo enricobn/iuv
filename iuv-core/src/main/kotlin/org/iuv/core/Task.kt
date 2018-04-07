ab31444563d29a236cd2d155e03e6595385ef3a5
Enrico Benedetti <enrico_benedetti@yahoo.com>
2018-04-07 08:48:51 +0200

    Simplified Task.

--- iuv-core/src/main/kotlin/org/iuv/core/Task.kt
+++ iuv-core/src/main/kotlin/org/iuv/core/Task.kt
@@ -1,31 +1,16 @@
 package org.iuv.core
 
 abstract class Task<out RESULT,ERROR> {
-    private val errorH : SubListenersHelper<ERROR> = SubListenersHelper()
-    private val successH : SubListenersHelper<RESULT> = SubListenersHelper()
 
-    fun <MESSAGE> perform(onSuccess: (RESULT) -> MESSAGE, onFailure: (ERROR) -> MESSAGE) : Cmd<MESSAGE> {
-        val cmd = toCmd(successH, onSuccess, errorH, onFailure)
-        start({ successH.dispatch(it) }, { errorH.dispatch(it) })
-        return cmd
+    fun <MESSAGE> perform(onSuccess: (RESULT) -> MESSAGE, onFailure: (ERROR) -> MESSAGE) = object : Cmd<MESSAGE> {
+        override fun run(messageBus: MessageBus<MESSAGE>) {
+            start({ messageBus.send(onSuccess(it)) }, { messageBus.send(onFailure(it)) })
+        }
     }
 
     fun <MESSAGE> run(onSuccess: (RESULT) -> Cmd<MESSAGE>, onFailure: (ERROR) -> Cmd<MESSAGE>) = object : Cmd<MESSAGE> {
         override fun run(messageBus: MessageBus<MESSAGE>) {
-            val subSuccess = successH.subscribe(onSuccess)
-            val subFailure = errorH.subscribe(onFailure)
-
-            val listener = object : SubListener<Cmd<MESSAGE>> {
-                override fun onMessage(message: Cmd<MESSAGE>) {
-                    subSuccess.removeListener(this)
-                    subFailure.removeListener(this)
-                    message.run(messageBus)
-                }
-            }
-            subSuccess.addListener(listener)
-            subFailure.addListener(listener)
-
-            start({ successH.dispatch(it) }, { errorH.dispatch(it) })
+            start({ onSuccess(it).run(messageBus) }, { onFailure(it).run(messageBus) })
         }
     }
 
@@ -42,24 +27,4 @@ abstract class Task<out RESULT,ERROR> {
         }
     }
 
-}
-
-private fun <RESULT,ERROR,MESSAGE> toCmd(successH : SubListenersHelper<RESULT>, onSuccess : (RESULT) -> MESSAGE,
-                                         failureH : SubListenersHelper<ERROR>, onFailure : (ERROR) -> MESSAGE) : Cmd<MESSAGE> {
-    val subSuccess = successH.subscribe(onSuccess)
-    val subFailure = failureH.subscribe(onFailure)
-
-    return (object : Cmd<MESSAGE> {
-        override fun run(messageBus: MessageBus<MESSAGE>) {
-            val listener: SubListener<MESSAGE> = object : SubListener<MESSAGE> {
-                override fun onMessage(message: MESSAGE) {
-                    subSuccess.removeListener(this)
-                    subFailure.removeListener(this)
-                    messageBus.send(message)
-                }
-            }
-            subSuccess.addListener(listener)
-            subFailure.addListener(listener)
-        }
-    })
 }
