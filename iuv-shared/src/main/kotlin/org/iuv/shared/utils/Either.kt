6f19094613b9e2017984b15f9c2d459a3dd35185
Enrico Benedetti <enrico_benedetti@yahoo.com>
2018-05-12 19:02:21 +0200

    Either.toTask.

--- iuv-shared/src/main/kotlin/org/iuv/shared/utils/Either.kt
+++ iuv-shared/src/main/kotlin/org/iuv/shared/utils/Either.kt
@@ -1,5 +1,7 @@
 package org.iuv.shared.utils
 
+import org.iuv.shared.Task
+
 /**
  * From https://github.com/adelnizamutdinov/kotlin-either
  * // TODO add dependency
@@ -7,7 +9,6 @@ package org.iuv.shared.utils
 @Suppress("unused")
 sealed class Either<out L, out R> {
     companion object {
-
         operator fun <L, R> invoke(left: L, right: R?) =
             if (right == null) Left(left) else Right(right)
     }
@@ -17,13 +18,18 @@ data class Left<out T>(val value: T) : Either<T, Nothing>()
 data class Right<out T>(val value: T) : Either<Nothing, T>()
 
 inline fun <L, R, T> Either<L, R>.fold(left: (L) -> T, right: (R) -> T): T =
-        when (this) {
-            is Left -> left(value)
-            is Right -> right(value)
-        }
+    when (this) {
+        is Left -> left(value)
+        is Right -> right(value)
+    }
 
 inline fun <L, R, T> Either<L, R>.flatMap(f: (R) -> Either<L, T>): Either<L, T> =
-        fold({ this as Left }, f)
+    fold({ this as Left }, f)
 
 inline fun <L, R, T> Either<L, R>.map(f: (R) -> T): Either<L, T> =
-        flatMap { Right(f(it)) }
+    flatMap { Right(f(it)) }
+
+fun <L, R> Either<L, R>.toTask(): Task<L, R> =
+    Task { onFailure, onSuccess ->
+        fold(onFailure, onSuccess)
+    }
