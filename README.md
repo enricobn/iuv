606f65de7b933934cbc92235dc4917a6b8d1c964
Enrico Benedetti <enrico_benedetti@yahoo.com>
2019-04-16 19:06:40 +0200

    Cli: newProject, fixed openAPI reading yaml files.

--- README.md
+++ README.md
@@ -15,8 +15,14 @@ The first time it will be slow since it will download node js, yarn and all java
 
 That task creates a "web" folder under iuv-examples/build, then you have to open index.html with a browser.  
 
+### Build command line tool
+From the command line : `./gradlew iuv-openapi-codegen:shadowJar`  
+
+An executable jar file in iuv-openapi-codegen/build/libs named *-all.jar should 
+be created.
+
 ### Install
 Since it's not in a remote repository, for now, you have to install locally. From the command line :  
 `./gradlew publishToMavenLocal`
 
-This task will install locally iuv-core.
+This task will install the libraries in the local maven repository.
--- iuv-openapi-codegen/src/main/kotlin/org/iuv/cli/MainCLI.kt
+++ iuv-openapi-codegen/src/main/kotlin/org/iuv/cli/MainCLI.kt
@@ -1,7 +1,10 @@
 package org.iuv.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
 import org.iuv.openapi.IUVAPI
 import org.iuv.openapi.OpenAPIReader
@@ -9,10 +12,18 @@ import org.iuv.openapi.OpenAPIWriteContext
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.FileWriter
+import java.io.InputStream
 
 private val LOGGER = LoggerFactory.getLogger(OpenAPICommand::class.java)
 
-class OpenAPICommand : CliktCommand() {
+class CLICommand : CliktCommand() {
+    override fun run() {
+
+    }
+
+}
+
+class OpenAPICommand : CliktCommand(name = "openAPI") {
     private val swaggerFilesFolder by argument(help="Swagger files folder").file(exists = true, fileOkay = false)
     private val controllerSourceFolder by argument(help="Controller source folder").file(exists = true, fileOkay = false)
     private val controllerPackage by argument(help="Controller package")
@@ -22,17 +33,16 @@ class OpenAPICommand : CliktCommand() {
     private val modelPackage by argument(help="Model package")
 
     override fun run() {
-    // iuv-openapi-codegen/src/test/resources/petstore-expanded.yaml
-
         val openAPIWriteContext = OpenAPIWriteContext(controllerPackage, clientPackage, modelPackage)
 
         getSwaggerFiles(swaggerFilesFolder)
             .forEach {
                 try {
+                    println("Processing file " + it.name)
                     val apiName = toApiName(it)
                     val api = OpenAPIReader.parse(it.toURI().toURL(), apiName)
                     if (api == null) {
-                        LOGGER.error("Error reading $it")
+                        System.err.println("Error reading $it")
                         return@forEach
                     } else {
                         runTemplate(controllerSourceFolder, controllerPackage, apiName + "Controller",
@@ -43,7 +53,8 @@ class OpenAPICommand : CliktCommand() {
                                 "/openapi/templates/components.mustache", api, openAPIWriteContext)
                     }
                 } catch (e: Exception) {
-                    LOGGER.error("Error reading $it", e)
+                    System.err.println("Error reading $it")
+                    e.printStackTrace(System.err)
                     return@forEach
                 }
             }
@@ -73,7 +84,7 @@ class OpenAPICommand : CliktCommand() {
     private fun getResource(resource: String) = this.javaClass.getResource(resource)
 
     private fun getSwaggerFiles(folder: File) : List<File> {
-        return folder.listFiles { it : File -> it.endsWith(".yaml") }.toList() +
+        return folder.listFiles { it : File -> it.extension == "yaml" }.toList() +
                 folder.listFiles { it: File -> it.isDirectory }.flatMap { getSwaggerFiles(it) }
     }
 
@@ -96,4 +107,107 @@ class OpenAPICommand : CliktCommand() {
     }
 }
 
-fun main(args: Array<String>) = OpenAPICommand().main(args)
+data class NewProjectContext(val group: String, val projectName: String, val kotlinVersion: String, val iuvVersion: String,
+                             val serializationVersion: String, val springBootVersion: String, val jarFileName: String) {
+
+    val clientPackage = "$group.client"
+
+}
+
+class NewProjectCommand : CliktCommand(name = "newProject") {
+    private val group: String by argument("group")
+    private val kotlinVersion : String by option().default("1.3.30")
+    private val iuvVersion : String by option().default("0.1-SNAPSHOT")
+    private val serializationVersion : String by option().default("0.9.1")
+    private val springBootVersion : String by option().default("2.0.1.RELEASE")
+    private val projectName = File(System.getProperty("user.dir")).name
+    private val jarFileName = File(this::class.java.protectionDomain.codeSource.location
+            .toURI()).path
+
+    override fun run() {
+        runTemplate("", ".gitattributes")
+        runTemplate("", ".gitignore")
+        runTemplate("", "build.gradle")
+
+        copyResource("gradlew", "gradlew")
+        copyResource("gradlew.bat", "gradlew.bat")
+
+        runTemplate("", "README.md")
+        runTemplate("", "settings.gradle")
+
+        runTemplate("shared", "build.gradle", destFolder = "$projectName-shared")
+        runTemplate("shared", "settings.gradle", destFolder = "$projectName-shared")
+
+        runTemplate("ui", ".gitignore", destFolder = "$projectName-ui")
+        runTemplate("ui", "build.gradle", destFolder = "$projectName-ui")
+        runTemplate("ui", "karma.conf.js", destFolder = "$projectName-ui")
+        runTemplate("ui", "package.json", destFolder = "$projectName-ui")
+        runTemplate("ui", "web.gradle", destFolder = "$projectName-ui")
+
+        runTemplate("ui/web", "index.html", destFolder = "$projectName-ui/web")
+        runTemplate("ui/web", "style.css", destFolder = "$projectName-ui/web")
+
+        runTemplate("web", "build.gradle", destFolder = "$projectName-web")
+
+        runTemplate("gradle/wrapper", "gradle-wrapper.properties")
+
+        runTemplate("", "openapi.sh")
+
+        File("openapi.sh").setExecutable(true)
+
+        File("openapi").mkdir()
+        File("$projectName-web/src/main/kotlin").mkdirs()
+        File("$projectName-web/src/test/kotlin").mkdirs()
+        File("$projectName-ui/src/main/kotlin").mkdirs()
+        File("$projectName-ui/src/test/kotlin").mkdirs()
+        File("$projectName-shared/src/commonMain/kotlin").mkdirs()
+        File("$projectName-shared/src/commonTest/kotlin").mkdirs()
+
+    }
+
+    private fun copyResource(resource: String, fileName: String) {
+        getResource("/project/templates/$resource").openStream().copyTo(File(fileName))
+    }
+
+    private fun runTemplate(folder: String, fileName: String, resource: String = fileName, destFolder: String = folder) {
+        val context = NewProjectContext(group, projectName, kotlinVersion, iuvVersion, serializationVersion,
+                springBootVersion, jarFileName)
+
+        val destFolderFile =
+            if (destFolder.isEmpty()) {
+                File(".")
+            } else {
+                File(destFolder)
+            }
+
+        val resourceFolder =
+                if (folder.isEmpty()) {
+                    "/project/templates"
+                } else {
+                    "/project/templates/$folder"
+                }
+
+        getOrCreateFileWriter(destFolderFile, fileName)
+            .use { writer ->
+                OpenAPIReader.runTemplate(getResource("$resourceFolder/$resource.mustache"), context, writer)
+            }
+    }
+
+    private fun getOrCreateFileWriter(folder: File, fileName: String): FileWriter {
+        assert(folder.mkdirs())
+        return FileWriter(File(folder, fileName))
+    }
+
+    private fun getResource(resource: String) = this.javaClass.getResource(resource)
+
+    private fun InputStream.copyTo(file: File) {
+        use { input ->
+            file.outputStream().use { fileOut ->
+                input.copyTo(fileOut)
+            }
+        }
+    }
+
+}
+
+fun main(args: Array<String>) = CLICommand().subcommands(NewProjectCommand(), OpenAPICommand()).main(args)
