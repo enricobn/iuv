bb9087d4ba576868040bf23b7ff9946219e4b70e
Enrico Benedetti <enrico_benedetti@yahoo.com>
2020-01-02 13:13:59 +0100

    MapRouteMatcher

--- iuv-core/src/jsMain/kotlin/org/iuv/core/IUVRouter.kt
+++ iuv-core/src/jsMain/kotlin/org/iuv/core/IUVRouter.kt
@@ -3,37 +3,65 @@ package org.iuv.core
 import org.w3c.dom.events.Event
 import kotlin.browser.window
 
-typealias IUVRoute<MODEL, MESSAGE, PARAMETERS> = (PARAMETERS) -> View<MODEL,MESSAGE>
+typealias IUVRoute<MODEL, MESSAGE, PARAMETERS> = (PARAMETERS) -> View<MODEL, MESSAGE>
 
+/**
+ * since implementations should implement equals, hashCode and toString perhaps they can be data classes.
+ */
 interface RouteMatcher<PARAMETERS> {
 
-    fun matches(absolutePath: String) : Boolean
+    /**
+     * @param absolutePath the full path with actual parameters
+     *
+     * @return true if the path can be handled by this matcher
+     *
+     * For example given the absolutePath http://hello.com#/order/customer001/order001, if this matcher handles /order
+     * relative path and two string parameters (customer001 and order001) it should return true
+     */
+    fun matches(absolutePath: String): Boolean
 
-    fun parameters(absolutePath: String) : Map<String,String>
+    /**
+     * @returns the list of parameter names and values given the full path with actual parameters
+     *
+     * Typically the parameter names are given to the constructor of the matcher and the values are captured from
+     * the absolutePath.
+     *
+     * For example given the absolutePath http://hello.com#/order/customer001/order001, probably the matcher is constructed
+     * with a relative path (/order) and two named parameters (for example customerId and orderId) then this method should
+     * return [("customerId", "customer001"), ("orderId", "order001")]
+     */
+    fun parameters(absolutePath: String): List<Pair<String, String>>
 
-    fun validate() : String?
+    // TODO it seems that it's not used
+    fun validate(): String?
 
-    fun toParam(parameters : Map<String,String>) : PARAMETERS
+    /**
+     * @return the object that represents the parameters given a list of single parameters names and values
+     */
+    fun toParam(parameters: List<Pair<String, String>>): PARAMETERS
 
-    fun link(parameter: PARAMETERS) : String
+    /**
+     * @return a relative path given the object that represents the parameters
+     */
+    fun link(parameter: PARAMETERS): String
 
+    /**
+     * @return a command that can be issued to go to the view given the object that represents the parameters
+     *
+     * It's a kind of helper function when the path is a bit complex to construct.
+     * TODO I don't know if it must be present in the base interface (if you remove it you probably should remove link either).
+     */
     fun <MESSAGE> goto(parameter: PARAMETERS) = IUVRouter.navigate<MESSAGE>(link(parameter))
 
+    override fun equals(other: Any?): Boolean
+
+    override fun hashCode(): Int
+
+    override fun toString(): String
 }
 
-//class RegexRouteMatcher(private val regex: Regex) : RouteMatcher {
-//
-//    override fun matches(absolutePath: String) =
-//        regex.matches(absolutePath)
-//
-//    override fun parameters(absolutePath: String): List<String> {
-//        return regex.find(absolutePath)?.groupValues ?: emptyList()
-//    }
-//
-//}
-
 abstract class AbstractRouteMatcher<P>(expression: String) : RouteMatcher<P> {
-    private val expComponents = expression.split("/")
+    protected val expComponents = expression.split("/")
 
     init {
         if (!expression.startsWith("/")) {
@@ -41,7 +69,7 @@ abstract class AbstractRouteMatcher<P>(expression: String) : RouteMatcher<P> {
         }
     }
 
-    final override fun matches(absolutePath: String) : Boolean {
+    final override fun matches(absolutePath: String): Boolean {
         if (absolutePath.contains(":")) return false
 
         val pathComponents = absolutePath.split("/")
@@ -51,38 +79,56 @@ abstract class AbstractRouteMatcher<P>(expression: String) : RouteMatcher<P> {
         return expComponents.zip(pathComponents).all { it.first.startsWith(":") || it.first == it.second }
     }
 
-    final override fun parameters(absolutePath: String): Map<String,String> {
+    final override fun parameters(absolutePath: String): List<Pair<String, String>> {
         val pathComponents = absolutePath.split(("/"))
 
         return expComponents
                 .zip(pathComponents)
-                .filter { it.first.startsWith(":")  }
+                .filter { it.first.startsWith(":") }
                 .map { Pair(it.first.substring(1), it.second) }
-                .toMap()
     }
 
     final override fun validate(): String? = null
 
 }
 
-class SimpleRouteMatcher(val path: String) : AbstractRouteMatcher<Unit>(path) {
+data class SimpleRouteMatcher(val path: String) : AbstractRouteMatcher<Unit>(path) {
 
     override fun link(parameter: Unit): String = path
 
-    override fun toParam(parameters: Map<String, String>) = Unit
+    override fun toParam(parameters: List<Pair<String, String>>) = Unit
 
 }
 
-class StringParameterMatcher(val path: String) : AbstractRouteMatcher<String>("$path/:id") {
+data class StringParameterMatcher(val path: String) : AbstractRouteMatcher<String>("$path/:id") {
 
     override fun link(parameter: String): String = "$path/$parameter"
 
-    override fun toParam(parameters: Map<String, String>): String = parameters["id"]!!
+    override fun toParam(parameters: List<Pair<String, String>>): String = parameters.first().second
+
+}
+
+data class MapRouteMatcher(private val expression: String) : AbstractRouteMatcher<LinkedHashMap<String, String>>(expression) {
+
+    override fun toParam(parameters: List<Pair<String, String>>): LinkedHashMap<String, String> =
+            linkedMapOf(*parameters.toTypedArray())
+
+    override fun link(parameter: LinkedHashMap<String, String>): String {
+        val parameterMap = parameter.toMap()
+
+        return expComponents.map {
+            if (it.startsWith(":")) {
+                parameterMap[it.substring(1)]!!
+            } else {
+                it
+            }
+        }.joinToString("/")
+    }
 
 }
 
 // Model
-data class RouterModel(val path : String, val currentModel: Any?, val errorMessage: String?)
+data class RouterModel(val path: String, val currentModel: Any?, val errorMessage: String?)
 
 // Messages
 interface RouterMessage
@@ -93,13 +139,14 @@ internal data class RouterMessageWrapper(val path: String, val childMessage: Any
 
 typealias MatcherAndRoute<MODEL, MESSAGE, PARAMETERS> =
         Pair<RouteMatcher<PARAMETERS>, IUVRoute<MODEL, MESSAGE, PARAMETERS>>
+
 /**
  * @param testMode if true no window history events are captured and the browser's location is not changed.
  */
-class IUVRouter(private val rootView: View<*,*>, val testMode : Boolean = false) : View<RouterModel, RouterMessage> {
-    private var routes = mutableListOf<MatcherAndRoute<*,*,*>>()
+class IUVRouter(private val rootView: View<*, *>, val testMode: Boolean = false) : View<RouterModel, RouterMessage> {
+    private var routes = mutableListOf<MatcherAndRoute<*, *, *>>()
     private var errorMessage: String? = null
-    private var baseUrl : String? = null
+    private var baseUrl: String? = null
 
     companion object {
         fun <MESSAGE> navigate(path: String) = object : Cmd<MESSAGE> {
@@ -114,7 +161,7 @@ class IUVRouter(private val rootView: View<*,*>, val testMode : Boolean = false)
         }
     }
 
-    fun <CHILD_MODEL,CHILD_MESSAGE,PARAMETERS> add(routeMatcher: RouteMatcher<PARAMETERS>, iuvRoute: IUVRoute<CHILD_MODEL, CHILD_MESSAGE, PARAMETERS>) {
+    fun <CHILD_MODEL, CHILD_MESSAGE, PARAMETERS> add(routeMatcher: RouteMatcher<PARAMETERS>, iuvRoute: IUVRoute<CHILD_MODEL, CHILD_MESSAGE, PARAMETERS>) {
         if (routes.any { routeMatcher == it.first }) {
             errorMessage = "Duplicate matcher: $routeMatcher."
             return
@@ -122,10 +169,10 @@ class IUVRouter(private val rootView: View<*,*>, val testMode : Boolean = false)
         routes.add(Pair(routeMatcher, iuvRoute))
     }
 
-    fun <CHILD_MODEL,CHILD_MESSAGE> add(path: String, view: View<CHILD_MODEL, CHILD_MESSAGE>) =
+    fun <CHILD_MODEL, CHILD_MESSAGE> add(path: String, view: View<CHILD_MODEL, CHILD_MESSAGE>) =
             add(SimpleRouteMatcher(path)) { view }
 
-    fun <CHILD_MODEL,CHILD_MESSAGE> add(routeMatcher: SimpleRouteMatcher, view: View<CHILD_MODEL, CHILD_MESSAGE>) =
+    fun <CHILD_MODEL, CHILD_MESSAGE> add(routeMatcher: SimpleRouteMatcher, view: View<CHILD_MODEL, CHILD_MESSAGE>) =
             add(routeMatcher) { view }
 
     override fun subscriptions(model: RouterModel): Sub<RouterMessage> {
@@ -133,19 +180,18 @@ class IUVRouter(private val rootView: View<*,*>, val testMode : Boolean = false)
             val (childView, error) = createChildView(model)
             if (childView != null) {
                 return childView.subscriptions(model)
+            } else if (error != null) {
+                console.error("Error creating child view for router: $error")
+            } else {
+                console.error("Unknown error creating child view for router")
             }
         }
         return Sub.none()
     }
 
-    override fun init() : Pair<RouterModel, Cmd<RouterMessage>> =
-        init(window.location.href)
+    override fun init(): Pair<RouterModel, Cmd<RouterMessage>> = init(window.location.href)
 
-    /**
-     * For test purposes
-     *
-     */
-    internal fun init(href: String) : Pair<RouterModel, Cmd<RouterMessage>> {
+    internal fun init(href: String): Pair<RouterModel, Cmd<RouterMessage>> {
         val (baseUrl, absolutePath) = parseHref(href)
 
         this.baseUrl = baseUrl
@@ -168,7 +214,7 @@ class IUVRouter(private val rootView: View<*,*>, val testMode : Boolean = false)
         )
     }
 
-    override fun update(message: RouterMessage, model: RouterModel) : Pair<RouterModel, Cmd<RouterMessage>> {
+    override fun update(message: RouterMessage, model: RouterModel): Pair<RouterModel, Cmd<RouterMessage>> {
         when (message) {
             is Goto -> {
                 val absolutePath =
@@ -180,19 +226,19 @@ class IUVRouter(private val rootView: View<*,*>, val testMode : Boolean = false)
 
                 if (!testMode && !message.fromBrowser) {
                     if (absolutePath == "/") {
-                        window.location.href = baseUrl + "#"
+                        window.location.href = "$baseUrl#"
                     } else {
-                        window.location.href = baseUrl + "#$absolutePath"
+                        window.location.href = "$baseUrl#$absolutePath"
                     }
                 }
 
                 val (childView, error) = createChildView(absolutePath)
 
-                if (childView == null) {
-                    return Pair(model.copy(path = absolutePath, errorMessage = error), Cmd.none())
+                return if (childView == null) {
+                    Pair(model.copy(path = absolutePath, errorMessage = error), Cmd.none())
                 } else {
                     val (childModel, cmd) = childView.init()
-                    return Pair(model.copy(path = absolutePath, currentModel = childModel, errorMessage = null), cmd)
+                    Pair(model.copy(path = absolutePath, currentModel = childModel, errorMessage = null), cmd)
                 }
 
             }
@@ -216,28 +262,30 @@ class IUVRouter(private val rootView: View<*,*>, val testMode : Boolean = false)
     }
 
     override fun view(model: RouterModel): HTML<RouterMessage> =
-        html {
-            errorMessage?.let {
-                +it
-                return@html
-            }
+            html {
+                errorMessage?.let {
+                    +it
+                    return@html
+                }
 
-            if (model.errorMessage != null) {
-                +model.errorMessage
-            } else {
-                val (childView, error) = createChildView(model)
-                if (childView != null) {
-                    childView.addTo(this, model)
+                if (model.errorMessage != null) {
+                    +model.errorMessage
                 } else {
-                    +error!!
+                    val (childView, error) = createChildView(model)
+                    if (childView != null) {
+                        childView.addTo(this, model)
+                    } else if (error != null) {
+                        +"Error in router: $error"
+                    } else {
+                        +"Unknown Error in router"
+                    }
                 }
             }
-        }
 
     /**
      * Returns a pair with baseURL and path
      */
-    private fun parseHref(href: String) : Pair<String, String> {
+    private fun parseHref(href: String): Pair<String, String> {
         val hash = href.indexOf("#/")
         return if (hash >= 0)
             Pair(href.substring(0, hash), href.substring(hash + 1))
@@ -245,7 +293,7 @@ class IUVRouter(private val rootView: View<*,*>, val testMode : Boolean = false)
             Pair(href, "/")
     }
 
-    private fun createChildView(absolutePath: String): Pair<ChildView<RouterModel, RouterMessage, Any, Any>?,String?> {
+    private fun createChildView(absolutePath: String): Pair<ChildView<RouterModel, RouterMessage, Any, Any>?, String?> {
         val childView: ChildView<RouterModel, RouterMessage, Any, Any>?
 
         if (absolutePath == "/") {
@@ -258,8 +306,8 @@ class IUVRouter(private val rootView: View<*,*>, val testMode : Boolean = false)
 
                 try {
                     childView = createChildView(absolutePath, route as MatcherAndRoute<*, *, Any>, parameters)
-                } catch (e: Exception) {
-                    return Pair(null, e.message)
+                } catch (e: Throwable) {
+                    return Pair(null, "Error creating route for matcher ${route.first}: $e")
                 }
             } else {
                 return Pair(null, "Cannot find path '$absolutePath'.")
@@ -275,7 +323,7 @@ class IUVRouter(private val rootView: View<*,*>, val testMode : Boolean = false)
             else
                 createChildView(model.path)
 
-    private fun createChildView(path: String, route: MatcherAndRoute<*,*,Any>, parameters: Map<String,String>): ChildView<RouterModel, RouterMessage, Any, Any> {
+    private fun createChildView(path: String, route: MatcherAndRoute<*, *, Any>, parameters: List<Pair<String, String>>): ChildView<RouterModel, RouterMessage, Any, Any> {
 
         val iuv = route.second.invoke(route.first.toParam(parameters))
 
--- iuv-core/src/jsTest/kotlin/org/iuv/core/IUVRouterTest.kt
+++ iuv-core/src/jsTest/kotlin/org/iuv/core/IUVRouterTest.kt
@@ -2,25 +2,6 @@ package org.iuv.core
 
 import kotlin.test.*
 
-// Model
-class Model(val id: Int)
-
-// Messages
-interface Message
-
-// IUV
-class SimpleView(val initialId: Int) : View<Model, Message> {
-
-    override fun init() : Pair<Model, Cmd<Message>> = Pair(Model(initialId), Cmd.none())
-
-    override fun view(model: Model): HTML<Message> =
-            html {+"SimpleView"}
-
-    override fun update(message: Message, model: Model): Pair<Model, Cmd<Message>> =
-            Pair(model, Cmd.none())
-
-}
-
 class IUVRouterTest {
 
     @Test
@@ -28,7 +9,7 @@ class IUVRouterTest {
         val router = IUVRouter(SimpleView(0), true)
 
         router.add("/simple", SimpleView(1))
-        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt())}
+        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt()) }
 
         val (model, _) = router.init()
         val (newModel, _) = router.update(Goto("/dummy", false), model)
@@ -41,7 +22,7 @@ class IUVRouterTest {
         val router = IUVRouter(SimpleView(0), true)
 
         router.add("/simple", SimpleView(1))
-        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt())}
+        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt()) }
 
         val (model, _) = router.init()
         val (newModel, _) = router.update(Goto("/simple0", false), model)
@@ -54,12 +35,14 @@ class IUVRouterTest {
         val router = IUVRouter(SimpleView(0), true)
 
         router.add("/simple", SimpleView(1))
-        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt())}
+        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt()) }
 
         val (model, _) = router.init()
         val (newModel, _) = router.update(Goto("/simple", false), model)
 
-        assertEquals(1, (newModel.currentModel as Model).id)
+        assertNull(newModel.errorMessage)
+
+        assertEquals(1, (newModel.currentModel as SimpleView.Model).id)
     }
 
     @Test
@@ -67,19 +50,21 @@ class IUVRouterTest {
         val router = IUVRouter(SimpleView(0), true)
 
         router.add("/simple", SimpleView(1))
-        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt())}
+        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt()) }
 
         val (model, _) = router.init()
         val (newModel, _) = router.update(Goto("/simple/5", false), model)
 
-        assertEquals(5, (newModel.currentModel as Model).id)
+        assertNull(newModel.errorMessage)
+
+        assertEquals(5, (newModel.currentModel as SimpleView.Model).id)
     }
 
     @Test
     fun givenARouteWithParameterWhenISendAGotoWithInvalidParameterThenThereIsAnErrorInRouterModel() {
         val router = IUVRouter(SimpleView(0), true)
 
-        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt())}
+        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt()) }
 
         val (model, _) = router.init()
 
@@ -94,7 +79,7 @@ class IUVRouterTest {
     fun givenARouteWithParameterWhenISendAGotoWithInvalidParameterAndISendAValidGotoThenThereIsNotAnErrorInRouterModel() {
         val router = IUVRouter(SimpleView(0), true)
 
-        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt())}
+        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt()) }
 
         val (model, _) = router.init()
         val (newModel, _) = router.update(Goto("/simple/hello", false), model)
@@ -103,29 +88,17 @@ class IUVRouterTest {
 
         val (newModel1, _) = router.update(Goto("/simple/1", false), newModel)
 
-        assertEquals(1, (newModel1.currentModel as Model).id)
+        assertEquals(1, (newModel1.currentModel as SimpleView.Model).id)
     }
 
     @Test
     fun givenARouteWithParameterWhenInitTheRouterWithAMatchingPathThenAGotoIsSentWithThePath() {
         val router = IUVRouter(SimpleView(0), true)
 
-        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt())}
+        router.add(StringParameterMatcher("/simple")) { SimpleView(it.toInt()) }
 
-        val (model, cmd) = router.init("http://hello.com#/simple/1")
-
-        assertNull(model.errorMessage)
-
-        val messages = mutableListOf<RouterMessage>()
-
-        val messagetBus = object : MessageBus<RouterMessage> {
-            override fun send(message: RouterMessage) {
-                messages += message
-            }
-
-        }
-
-        cmd.run(messagetBus)
+        val messages = initRouterAndCaptureMessages(router,
+                "http://hello.com#/simple/1")
 
         assertEquals(Goto("/simple/1", true), messages.first())
     }
@@ -137,12 +110,96 @@ class IUVRouterTest {
         var exception: Exception? = null
 
         try {
-            router.add(StringParameterMatcher("simple")) { SimpleView(it.toInt())}
+            router.add(StringParameterMatcher("simple")) { SimpleView(it.toInt()) }
         } catch (e: Exception) {
             exception = e
         }
 
         assertNotNull(exception)
     }
+
+    @Test
+    fun givenAMapRouteMatcherWhenInitTheRouterWithAMatchingPathThenAGotoIsSentWithThePath() {
+        val router = IUVRouter(SimpleView(0), true)
+
+        router.add(MapRouteMatcher("/order/:customerId/:orderId")) { OrderView(it["customerId"]!!, it["orderId"]!!) }
+
+        val messages = initRouterAndCaptureMessages(router,
+                "http://hello.com#/order/customer001/order001")
+
+        assertEquals(Goto("/order/customer001/order001", true), messages.first())
+    }
+
+    @Test
+    fun givenAMapRouteMatcherWhenISendAGotoWithParameterThenTheParametersMustBePresentInView() {
+        val router = IUVRouter(SimpleView(0), true)
+
+        router.add(MapRouteMatcher("/order/:customerId/:orderId")) { OrderView(it["customerId"]!!, it["orderId"]!!) }
+
+        val (model, _) = router.init()
+        val (newModel, _) = router.update(Goto("/order/customer001/order001", false), model)
+
+        assertNull(newModel.errorMessage)
+
+        assertEquals("customer001", (newModel.currentModel as OrderView.Model).customerId)
+        assertEquals("order001", (newModel.currentModel as OrderView.Model).orderId)
+    }
+
+    @Test
+    fun testMapRouterMatcherLink() {
+        val matcher = MapRouteMatcher("/customer/:customerId/order/:orderId")
+
+        val link = matcher.link(linkedMapOf(Pair("customerId", "customer001"), Pair("orderId", "order001")))
+
+        assertEquals("/customer/customer001/order/order001", link)
+    }
+
+    private fun initRouterAndCaptureMessages(router: IUVRouter, href: String): MutableList<RouterMessage> {
+        val (model, cmd) = router.init(href)
+
+        assertNull(model.errorMessage)
+
+        val messages = mutableListOf<RouterMessage>()
+
+        val messageBus = object : MessageBus<RouterMessage> {
+            override fun send(message: RouterMessage) {
+                messages += message
+            }
+
+        }
+
+        cmd.run(messageBus)
+        return messages
+    }
+
+}
+
+class SimpleView(private val initialId: Int) : View<SimpleView.Model, SimpleView.Message> {
+
+    class Model(val id: Int)
+
+    interface Message
+
+    override fun init(): Pair<Model, Cmd<Message>> = Pair(Model(initialId), Cmd.none())
+
+    override fun view(model: Model): HTML<Message> = html { }
+
+    override fun update(message: Message, model: Model): Pair<Model, Cmd<Message>> =
+            Pair(model, Cmd.none())
+
+}
+
+class OrderView(private val customerId: String, private val orderId: String) : View<OrderView.Model, OrderView.Message> {
+
+    data class Model(val customerId: String, val orderId: String)
+
+    interface Message
+
+    override fun init(): Pair<Model, Cmd<Message>> = Pair(Model(customerId, orderId), Cmd.none())
+
+    override fun view(model: Model): HTML<Message> = html { }
+
+    override fun update(message: Message, model: Model): Pair<Model, Cmd<Message>> =
+            Pair(model, Cmd.none())
 
 }
