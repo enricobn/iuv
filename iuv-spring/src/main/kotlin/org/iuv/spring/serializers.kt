e3983600eab4db92977dc8d0c83bd757f0940c53
Enrico Benedetti <enrico_benedetti@yahoo.com>
2020-09-14 15:24:35 +0200

    KClassSerializer

--- iuv-spring/src/main/kotlin/org/iuv/spring/serializers.kt
+++ iuv-spring/src/main/kotlin/org/iuv/spring/serializers.kt
@@ -1,7 +1,10 @@
 package org.iuv.spring
 
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.serializer
+import kotlin.reflect.KClass
 
 object IntIUVSerializer : IUVSerializer {
     override val serializer: KSerializer<*>
@@ -16,4 +19,12 @@ object UnitIUVSerializer : IUVSerializer {
 object StringIUVSerializer : IUVSerializer {
     override val serializer: KSerializer<*>
         get() = String.serializer()
+}
+
+@InternalSerializationApi
+open class KClassSerializer(private val kClass: KClass<*>) : IUVSerializer {
+
+    override val serializer: KSerializer<*>
+        get() = kClass.serializer()
+
 }
